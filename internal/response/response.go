d3c63a53f514202cfb402c6742696829fd3388b5
Biruk Tesfaye <biruktesfayeakabu@gmail.com>
2026-06-11 23:54:44 +0300

    feat: add JSON helper function for standardized HTTP response encoding

--- internal/response/response.go
+++ internal/response/response.go
@@ -1,9 +1,29 @@
 package response
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type ErrorResponse struct {
 	Error string `json:"error"`
 }
 
 type SuccessResponse struct {
 	Message string `json:"message"`
+}
+
+func JSON(
+	w http.ResponseWriter,
+	status int,
+	data any,
+) {
+	w.Header().Set(
+		"Content-Type",
+		"application/json",
+	)
+
+	w.WriteHeader(status)
+
+	json.NewEncoder(w).Encode(data)
 }
